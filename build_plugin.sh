07ca833a4c29670ad11329266cd198a2f692f25a
Christian Domínguez <christian.99dominguez@gmail.com>
2024-09-10 10:13:30 +0200

    Updated build script

--- build_plugin.sh
+++ build_plugin.sh
@@ -2,6 +2,12 @@
 
 echo "Building plugin..."
 
+current_version=$(git describe --tags)
+
+echo "Current version: $current_version"
+
+sed -i "s/\"version\": \"[^\"]*\"/\"version\": \"$current_version\"/" set_all_remotes/plugin.meta
+
 rm set_all_remotes.hp
 
 zip -r set_all_remotes.hp set_all_remotes
